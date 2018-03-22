09f6a4b42688f481392b714b2bbcebbf199489a9
xpiqiu <1156559005@qq.com>
2018-03-22 16:19:34 +0800

    second

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -1,24 +1,31 @@
 package mg.studio.weatherappdesign;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-<<<<<<< HEAD
-
-=======
 import java.util.Calendar;
->>>>>>> 1
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,82 +33,129 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Net(this))
+            new DateUpdate().execute();
+        else
+            Toast.makeText(this, "网络不可用,无法连接", Toast.LENGTH_SHORT).show();
     }
-<<<<<<< HEAD
-
+    public boolean Net(Context context){
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null && info.isConnected())
+            {
+                // 当前所连接的网络可用
+                //Toast.makeText(this, "网络可用", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        }
+        //Toast.makeText(this, "网络不可用", Toast.LENGTH_SHORT).show();
+        return false;
+    }
     public void btnClick(View view) {
-        new DownloadUpdate().execute();
-    }
 
-
-=======
-    public void btnClick(View view) {
-        Calendar calendar = Calendar.getInstance();//获取系统的日期
-        int year = calendar.get(Calendar.YEAR);//年
-        int month = calendar.get(Calendar.MONTH);//月
-        int day = calendar.get(Calendar.DAY_OF_MONTH);//日
-        int day_of_week =calendar.get(Calendar.DAY_OF_WEEK);//星期
-        String[] weeks = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
-        ((TextView) findViewById(R.id.dayoftheweek)).setText(weeks[day_of_week-1]);
-        ((TextView) findViewById(R.id.tv_date)).setText((month+1)+"/"+day+"/"+year);
-        new DownloadUpdate().execute();
+        if(Net(this))
+            new DateUpdate().execute();
+        else
+            Toast.makeText(this, "网络不可用,无法连接", Toast.LENGTH_SHORT).show();
     }
->>>>>>> 1
-    private class DownloadUpdate extends AsyncTask<String, Void, String> {
+    private class DateUpdate extends AsyncTask<String, Void, String> {
 
 
         @Override
-        protected String doInBackground(String... strings) {
-            String stringUrl = "http://mpianatra.com/Courses/info.txt";
+        protected String doInBackground(String... strings){
+            String str="";
+            String response="";
+            final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=101040100";
             HttpURLConnection urlConnection = null;
-            BufferedReader reader;
+            Calendar date = Calendar.getInstance();
+            int day = date.get(Calendar.DAY_OF_MONTH);
 
             try {
-                URL url = new URL(stringUrl);
-
-                // Create the request to get the information from the server, and open the connection
+                URL url = new URL(address);
                 urlConnection = (HttpURLConnection) url.openConnection();
-
                 urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
+                urlConnection.setConnectTimeout(8000);
+                urlConnection.setReadTimeout(8000);
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuffer s = new StringBuffer();
 
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
+                while((str=reader.readLine())!=null)
+                    s.append(str);
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Mainly needed for debugging
-                    buffer.append(line + "\n");
-                }
+                response = s.toString();
+                Log.d("response",response);
 
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                //The temperature
-                return buffer.toString();
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            }catch (Exception e)
+            {
                 e.printStackTrace();
             }
 
-            return null;
-        }
+            StringBuilder dataBack = new StringBuilder();
 
+            //解析XML数据
+            try {
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser xmlPullParser = factory.newPullParser();
+                xmlPullParser.setInput(new StringReader(response));
+                int eventType = xmlPullParser.getEventType();
+                String date2= "";
+                String high = "";
+                String low = "";
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    String nodeName = xmlPullParser.getName();
+                    switch (eventType) {
+                        //  开始解析某个结点
+                        case XmlPullParser.START_TAG: {
+                            if ("date".equals(nodeName)) {
+                                date2 = xmlPullParser.nextText();
+                            } else if ("high".equals(nodeName)) {
+                                high = xmlPullParser.nextText();
+                            } else if ("low".equals(nodeName)) {
+                                low = xmlPullParser.nextText();
+                            }
+
+                            break;
+                        }
+
+                        default:
+                            break;
+                    }
+
+                    if(date2.contains( day+"日") && !high.isEmpty() && !low.isEmpty()){
+                        dataBack.append(date+"/");
+                        dataBack.append(high+"/");
+                        dataBack.append(low);
+                        break;
+                    }
+                    eventType = xmlPullParser.next();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            String data = dataBack.toString();
+
+            return data;
+        }
         @Override
-        protected void onPostExecute(String temperature) {
-            //Update the temperature displayed
-            ((TextView) findViewById(R.id.temperature_of_the_day)).setText(temperature);
+
+        protected void onPostExecute(String data) {
+            Calendar calendar = Calendar.getInstance();//获取系统的日期
+            //字符串切割
+            String[] temperature = data.split("/");
+
+            //当日的最高温和最低温
+            String highTemp = temperature[1].substring(3,5);
+            String lowTemp  = temperature[2].substring(3,5);
+            int year = calendar.get(Calendar.YEAR);//年
+            int month = calendar.get(Calendar.MONTH);//月
+            int day = calendar.get(Calendar.DAY_OF_MONTH);//日
+            int day_of_week =calendar.get(Calendar.DAY_OF_WEEK);//星期
+            String[] weeks = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
+            ((TextView) findViewById(R.id.dayoftheweek)).setText(weeks[day_of_week-1]);
+            ((TextView) findViewById(R.id.tv_date)).setText((month+1)+"/"+day+"/"+year);
+            ((TextView) findViewById(R.id.temperature_of_the_day)).setText(lowTemp+"~"+highTemp);
         }
     }
 }
